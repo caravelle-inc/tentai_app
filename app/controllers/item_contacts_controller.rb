e15429aed9e901e4db8263db0ded0b943d52022b
seigooo <m220274@gmail.com>
2015-10-22 22:03:48 +0900

    Actionmailer

--- app/controllers/item_contacts_controller.rb
+++ app/controllers/item_contacts_controller.rb
@@ -6,7 +6,11 @@ class ItemContactsController < ApplicationController
 
   def create
     @item_contact = ItemContact.new item_contact_params
+    @item = Item.find(@item_contact.item_id)
+    @item_admin = @item.admin_user
     if @item_contact.save
+      ItemContactMailer.item_contact_mail(@item_contact, @item).deliver_now
+      ItemContactMailer.item_contact_to_admin(@item_contact, @item_admin).deliver_now
       flash[:notice] = "お問い合わせが完了しました"
       redirect_to items_path
     else
