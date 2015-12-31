22548f5082fdc535efea6dc1988023b41239734d
seigooo <m220274@gmail.com>
2015-12-31 19:26:01 +0900

    integer USING CAST(price AS integer)

--- db/migrate/20151208134320_change_datatype_price_of_items.rb
+++ db/migrate/20151208134320_change_datatype_price_of_items.rb
@@ -1,5 +1,14 @@
 class ChangeDatatypePriceOfItems < ActiveRecord::Migration
   def change
-    change_column :items, :price, :integer, :null => false
+    case Rails.env
+      when 'production'
+        change_column :items, :price, 'integer USING CAST(price AS integer)', :null => false
+      when 'staging'
+        change_column :items, :price, :integer, :null => false
+      when 'development'
+        change_column :items, :price, :integer, :null => false
+      when 'test'
+        change_column :items, :price, :integer, :null => false
+    end
   end
 end
